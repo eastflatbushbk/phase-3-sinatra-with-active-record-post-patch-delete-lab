d50dd2e31a0de8d4745e4aefc9647ec5de92f8ee
Robert Sainton <robertsainton@outlook.com>
2022-12-22 16:33:42 -0500

    completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < Sinatra::Base
   set default_content_type: "application/json"
+
+  delete '/baked_goods/:id' do
+     pastries = BakedGood.find(params[:id])
+     pastries.destroy
+     pastries.to_json
+  end
+
+  post '/baked_goods' do 
+    pastry = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+      ) 
+      pastry.to_json
+  end
+
+  patch '/bakeries/:id' do
+    bakeries = Bakery.find(params[:id])
+
+    bakeries.update( name: params[:name])
+
+    bakeries.to_json
+  end
   
   get '/bakeries' do
     bakeries = Bakery.all
